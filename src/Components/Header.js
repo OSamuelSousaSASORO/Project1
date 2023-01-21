fdf6fcb4f3550c530acb7f9560cbbb2e4e8e8ba1
OSamuelSousaSASORO <osamuelsousa@outlook.com>
2023-01-21 19:26:59 -0300

    Continuando o projeto, Add(Botão hamburguer funcinoando)

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,102 @@
 import React from "react";
 import styles from './Header.module.css'
 import foto from '../Photo/logoBlack.png'
+import { useRef } from "react";
 
 export default function Header(){
+    
+    const button1 = useRef()
+    const button2 = useRef()
+    const button3 = useRef()
+    const button4 = useRef()
+    const button5 = useRef()
+    const button6 = useRef()
+    const button7 = useRef()
+    const menuHidden = useRef()
+    const close = useRef()
+    const menuScreenVisible = useRef()
+
+    function visibleOBJ(){
+        menuHidden.current.style.left = '0px';
+        button1.current.style.left = '0px';
+        button2.current.style.left = '0px';
+        button3.current.style.left = '0px';
+        button4.current.style.left = '0px';
+        button5.current.style.left = '0px';
+        button6.current.style.left = '0px';
+        button7.current.style.left = '0px';
+    }
+
+    function hiddenOBJ(){
+        menuHidden.current.style.left = '-100%';
+        button1.current.style.left = '-150px';
+        button2.current.style.left = '-150px';
+        button3.current.style.left = '-150px';
+        button4.current.style.left = '-150px';
+        button5.current.style.left = '-150px';
+        button6.current.style.left = '-150px';
+        button7.current.style.left = '-150px';
+    }
+
+    function menuHiddenVisible(){
+        visibleOBJ()
+    }
+
+    window.addEventListener('click', (e)=>{
+        if (e.target === menuScreenVisible.current || e.target === close.current) {
+            hiddenOBJ()
+        }
+    })
+
     return (
         <>
             <div className={styles.container}>
                 <div className={styles.header}>
-                    <div className={styles.menu}>
+                    <div className={styles.menu} onClick={()=>{menuHiddenVisible()}}>
                         <i className={styles.ione}></i>
                         <i className={styles.itwo}></i>
                     </div>
                     <img src={foto} alt='' className={styles.logo}></img>
-                    <input placeholder="Encontre uma concessionária" className={styles.input}/>
-                    <i></i>
+                    <div className={styles.ourmodels}>Nossos modelos</div>
                 </div>
             </div>
+            <div className={styles.menuHidden} ref={menuHidden} >
+                <div className={styles.hiddenBox}>
+                    <div className={styles.first}>
+                        <i ref={close}>X</i>
+                        <button ref={button1}>Contato</button>
+                        <button ref={button2}>Modelos</button>
+                        <button ref={button3}>Compre agora</button>
+                        <button ref={button4}>Sustentabilidade</button>
+                        <button ref={button5}>Visão geral</button>
+                        <button ref={button6}>Noticias FK</button>
+                        <button ref={button7}>A empresa FK</button>
+                    </div>
+                    <div className={styles.secound}>
+                        <button>Estoque</button>
+                        <button>Encontre uma concessionária</button>
+                    </div>
+                </div>
+                <div className={styles.space} ref={menuScreenVisible}></div>
+            </div>
+
+            <datalist id="testes">
+                <option value='Caxias do Sul - RS'/>
+                <option value='Porto Alegre - RS'/>
+                <option value='Canela - RS'/>
+                <option value='Gramado - RS'/>
+                <option value='Torres - RS'/>
+                <option value='Curitiba - PR'/>
+                <option value='Londrina - PR'/>
+                <option value='Maringá - PR'/>
+                <option value='São José dos Pinhais - PR'/>
+                <option value='Cascavel - PR'/>
+                <option value='Florianópolis - SC'/>
+                <option value='Blumenau - SC'/>
+                <option value='Joinville - SC'/>
+                <option value='Balneário - SC'/>
+                <option value='São José - SC'/>
+            </datalist>
         </>
     )
 }
--- src/Components/Section1.js
+++ src/Components/Section1.js
@@ -1,27 +1,70 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useRef } from 'react'
 import styles from './Section1.module.css'
 
 export default function Section1 (){
+
+    const container2 = useRef()
+
+    useEffect(()=>{
+        const observer = new IntersectionObserver((entry)=>{
+            console.log(entry)
+        })
+        observer.observe(container2.current)
+    }, [])
+
+    function smothscroll(){
+        container2.current.scrollIntoView({behavior: "smooth"})
+    }
 
     return(
         <>
             <div className={styles.container}>
                 <div className={styles.nurb}></div>
                 <div className={styles.title}></div>
-                <div className={styles.description}>
-                    <p className={styles.strongtxt}>Lendas</p> são feitas atráves da busca pela perfeição.
-                    <br/><br/>
-                    Esse monstro vem equipado com motor central traseiro que representa o ápice da performance. Marcando incríveis <p className={styles.strongtxt}>6:48:32s</p> na pista de <p className={styles.strongtxt}>Nurburgring</p>.
+                <div className={styles.carAndDescription}>
+                    <div className={styles.car}></div>
+                    <div className={styles.boxdescription}>
+                        <div className={styles.description}>
+                        &nbsp;&nbsp;&nbsp;&nbsp;<p className={styles.strongtxt}>Lendas</p> são feitas atráves da busca pela perfeição.
+                            <br/><br/>
+                            &nbsp;&nbsp;&nbsp;&nbsp;Esse monstro vem equipado com motor central traseiro que representa o ápice da performance. Marcando incríveis <p className={styles.strongtxt}>6:48:32s</p> na pista de <p className={styles.strongtxt}>Nurburgring</p>.
+                        </div>
+                        <div className={styles.boxbuttons}>
+                            <p className={styles.viewcar} onClick={()=>smothscroll()} >Ver o modelo</p>
+                            <p className={styles.buildyourcar}>Monte o seu</p>
+                        </div>
+                    </div>
                 </div>
-                <div className={styles.car}></div>
-                <div className={styles.boxmodels}>
-                    <Link to='/' className={styles.viewcar}>Ver o modelo</Link>
-                    <Link to='/' className={styles.buildyourcar}>Monte o seu</Link>
-                </div>
-                <div className={styles.scroll}>
+                <div className={styles.scroll} ref={container2}>
                     <i className={styles.scrollt}></i>
                     <i className={styles.scrolld}></i>
+                </div>
+            </div>
+            <div className={styles.container2}>
+                <div className={styles.car2}></div>
+                <div className={styles.description2}>
+                    <h1>Desempenho máximizado</h1>
+                    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ao ver o sinal verde, esse Ford ka exibe sua impressionante capacidade de desempenho no circuito, graças a seus 510 Nm de torque e 412 kW (560 cv). Ele é capaz de acelerar de 0 a 100 km/h em apenas 2,9 segundos, alcançando uma velocidade máxima de 305 km/h antes que você possa piscar. O sistema de escape esportivo leve de aço inoxidável garante uma experiência sonora autêntica até o giro máximo de 12.000 rpm.</p>
+                </div>
+                <div className={styles.description3}>
+                    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;O Ford Ka possui um motor de 4,2 litros de alta velocidade, tecnologia de 4 válvulas, incluindo balancins, comando de válvulas, bielas de titânio, pistões forjados, alimentação de óleo otimizada e lubrificação por cárter seco com tanque separado para óleo do motor preparado para forças laterais elevadas. Além disso, para alcançar os melhores tempos possíveis, 6 válvulas individuais garantem a alimentação de ar perfeita em cada cilindro.</p>
+                </div>
+                <div className={styles.times}>
+                    <h1>Recorde nas pistas:</h1>
+                    <div>
+                        <div>
+                            <p>2:10.53 &nbsp; Spa-Francorchamps</p>
+                            <p>1:51.29 &nbsp; Suzuka</p>
+                            <p>1:54.34 &nbsp; Mônaco</p>
+                            <p>2:31.28 &nbsp; Interlagos</p>
+                        </div>
+                        <div>
+                            <p>1:40.47 &nbsp; Silverstone</p>
+                            <p>2:32.15 &nbsp; Bahrain</p>
+                            <p>1:30.07 &nbsp; Fuji</p>
+                            <p>1:45.45 &nbsp; Catalunya</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
